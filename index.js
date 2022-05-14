82f65fad47295cbf6710c981eb6c56f7d3d7b63e
Andres <andresfelipeacostagarcia34@gmail.com>
2022-05-14 11:22:58 -0500

    commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('port', process.env.port || 8000);
+let router = express.Router();
+app.use(express.static('public'));
+app.set('view engine', 'pug')
+
+router
+  .get('/', function(req, res){
+    res.render("index")
+  })
+  .post('/name', (req, res)=>{
+    console.log(req.body.nombre)
+    res.send("hi")
+
+  })
+
+
+app.use(router)
+app.listen(app.get('port'), function(){
+    console.log("Conectado al puerto: ", app.get('port'))
+  });
