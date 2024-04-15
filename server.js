2ff44ed5f5d54af2c1acf64a454e44c847b232a7
andres acosta garcia <andresfelipeacostagarcia34@gmail.com>
2024-04-14 23:44:52 -0500

    added the cooking fun facts section and the model to get random cooking techniques in the cooking techniques section

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 let express = require('express'),
     app = express(),
+    cookingTechniques = require("./dao_layer/models").cookingTechniques
     bodyParser = require('body-parser'),
     router = express.Router()
 
@@ -7,13 +8,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.port || 8000);
 app.use(express.static('public'));
 app.set('view engine', 'pug')
+require('dotenv').config()
+
+const apiKey = process.env.API_KEY;
+
 
 router
   .get('/', function(req, res){
-    res.render("index")
+    res.render("index", {apiKey:apiKey})
   })
- 
-
+  .get('/facts', function(req, res){
+    res.render("facts")
+  })
+  .get('/techniques', async function(req, res){
+    let techniques = await cookingTechniques.find().limit(10)
+    console.log(techniques)
+    res.render("techniques")
+  })
 
 app.use(router)
 app.listen(app.get('port'), function(){
