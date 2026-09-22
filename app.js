6692ccb649390d4ccc556749e9b0337047cc7bff
andres acosta <andresfelipeacostagarcia34@gmail.com>
2026-09-22 09:50:39 -0500

    add: ingredients based recipes route

--- app.js
+++ app.js
@@ -10,16 +10,9 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug')
-if (process.env.NODE_ENV !== 'production') {
-  app.disable('view cache');
-}
+app.disable('view cache');
 
 require('dotenv').config()
-
-const apiKey = process.env.SPOONACULAR_API_KEY || process.env.API_KEY;
-// Accept common misspelling `DEESEEK_API_KEY` as well as `DEEPSEEK_API_KEY`.
-const deepseekApiKey = process.env.DEEPSEEK_API_KEY || process.env.DEESEEK_API_KEY || '';
-
 
 router
   .get('/', function(req, res){
